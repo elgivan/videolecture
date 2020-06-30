0ef280115bbc5b7cbb4ec1ad798e620bd2b3459d
Eleni Givanoudi <elgivan@gmail.com>
2020-06-30 19:40:29 +0300

    add website

---
+++ lecture4-2/config.js
@@ -0,0 +1,115 @@
+const questions =
+[
+  {
+    question: "",
+    correct: 2,
+    answers: 
+    ["Ενσύρματη.","Ασύρματη."],
+    stoptimer: 498 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Ενσύρματη.","Ασύρματη."],
+    stoptimer: 529 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Ενσύρματη.","Ασύρματη."],
+    stoptimer: 547 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 2,
+    answers: 
+    ["Ενσύρματη.","Ασύρματη."],
+    stoptimer: 564 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 635 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 648 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 2,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 658 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 674 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 684 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 696 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 708 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 2,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 721 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 736 ,
+    done: false
+  },
+  {
+    question: "",
+    correct: 1,
+    answers: 
+    ["Σ (σωστό)","Λ (λάθος)"],
+    stoptimer: 747 ,
+    done: false
+  }
+];
---
+++ lecture4-3/main.js
@@ -0,0 +1,76 @@
+window.onload = function()
+{
+  let video = document.getElementById("video");
+  let questionBox = document.getElementById("question");
+  let wrongSound = new Audio("wrong.wav");
+  let correctSound = new Audio("correct.wav");
+  let wasFullscreen = false;  
+
+  video.addEventListener("timeupdate", function()
+  {
+    let ct = this.currentTime;
+    for(let i = 0; i < questions.length; ++i)
+    {
+      let q = questions[i];
+      if(q.done){ continue; }
+      if(q.stoptimer - 0.5 < ct && ct < q.stoptimer + 0.5)
+      {
+        this.pause();
+        if(document.fullscreenElement)
+        {
+          document.exitFullscreen();
+          wasFullscreen = true;
+        }
+        showQuestion(q);
+        questionBox.scrollIntoView();
+      }
+    }
+  });
+
+  function showQuestion(q)
+  {
+    questionBox.classList.remove("hidden");
+    let oldButtons = questionBox.querySelectorAll("button");
+    for(let i = 0; i < oldButtons.length; ++i)
+    {
+      questionBox.removeChild(oldButtons[i]);
+    }
+
+    let questionLabel = questionBox.querySelector("span");
+    questionLabel.innerText = q.question;
+    for(let i = 0; i < q.answers.length; ++i)
+    {
+      let b = document.createElement("button");
+      b.innerText = q.answers[i];
+      b.answer = i + 1;
+      b.addEventListener("click", function()
+      {
+        let buttons = questionBox.querySelectorAll("button");
+        for(let i = 0; i < buttons.length; ++i)
+        {
+          buttons[i].classList.remove("wrong");
+        }
+       
+        if(this.answer == q.correct)
+        {
+          correctSound.play();
+          questionBox.classList.add("hidden");
+          q.done = true;
+          if(wasFullscreen)
+          {
+            video.requestFullscreen();
+            wasFullscreen = false;
+          }
+          video.play();
+        }
+        else
+        {
+          this.classList.add("wrong");
+          wrongSound.play();
+        }
+      });
+      questionBox.appendChild(b);
+    }
+  }
+    
+};
